b6696e9631029376bc3405f4589fd68acd6559dd
Brutal-45 <creatorsports81@gmail.com>
2026-04-14 20:33:54 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
+export const db = 
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['query'],
