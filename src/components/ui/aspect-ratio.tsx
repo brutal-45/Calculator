0893d1996c886dfba7511053717c094cd2137016
brutal-45 <creatorsports81@gmail.com>
2026-04-17 11:05:30 +0530

    Update aspect-ratio.tsx

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -6,6 +6,6 @@ function AspectRatio({
   ...props
 }: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
   return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />
-}
+} 
 
 export { AspectRatio }
