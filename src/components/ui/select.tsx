7433d223c72142b3c2e065cd7b81d6f6a7eb6208
brutal-45 <creatorsports81@gmail.com>
2026-04-17 11:09:12 +0530

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -5,7 +5,7 @@ import * as SelectPrimitive from "@radix-ui/react-select"
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-
+ 
 function Select({
   ...props
 }: React.ComponentProps<typeof SelectPrimitive.Root>) {
